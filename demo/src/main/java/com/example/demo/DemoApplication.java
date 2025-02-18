89d045b78094d24a1bd68326b25d6f8a39e5a101
mateomate1 <mateoayarrabarbero@gmail.com>
2025-02-18 11:44:28 +0100

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,32 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+	
+	private AlumnoService servicioAlumno;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	public DemoApplication(AlumnoService servicioAlumno) {
+		this.servicioAlumno = servicioAlumno;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//Aqui añadiremos todas las aplicaciones antes de añadirlas al main
-		System.out.println("Hola");
+		//Aqui añadiremos todas las aplicaciones antes de añadirlas al main()
+		Alumno al = new Alumno(1000, "Manuel Díaz", LocalDate.of(1990, 12, 31), null);
+		Contacto con = new Contacto(null, null, al);
 	}
 
 }
